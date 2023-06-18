f6dbb84cde2658ba8eb5388478da96e68b0b9b82
NikoMalek <niko.malek.aleuanlli@live.com>
2023-06-18 18:05:14 -0400

    Implemented Product Details Page

--- app/controllers/RoutesController.java
+++ app/controllers/RoutesController.java
@@ -8,5 +8,10 @@ public class RoutesController extends Controller{
     public Result phone(){
         return ok(views.html.phone.render());
     }
-
+    public Result spidey(){
+        return ok(views.html.spidey.render());
+    }
+    public Result leon(){
+        return ok(views.html.leon.render());
+    }
 }
