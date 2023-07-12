f393b99fb63bd0abba0c2f21c655ef823f4f989a
Camilo Ojeda <camilomiguel.ojeda@alumnos.ulagos.cl>
2023-07-12 15:29:18 -0400

    Añadido ficha pdf de la figura guerrero leon

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -6,6 +6,7 @@
 -- apply changes
 create table user (
   id                            integer not null,
+  name                          varchar(255),
   email                         varchar(255),
   password                      varchar(255),
   constraint pk_user primary key (id)
